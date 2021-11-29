6f7726e0dc661521a54635cfea40c1876750673f
Patrick Auri <patrickaulie@gmail.com>
2021-11-29 11:03:06 +0100

    Added DB & read from DB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+const db = require("./db/NHKdata.json")
 
 function App() {
+  const [pitch, setPitch] = useState([])
+
+  const getPitch = (e) => {
+    // Get all results including the query
+    const results = db.filter(
+      (item) =>
+        item["Word 1A"].includes(e) &&
+        item["Word 1A"][item["Word 1A"].indexOf(e) - 1] === "【"
+    )
+
+    if (results) {
+      console.table(results)
+      setPitch(results)
+    } else {
+      console.log("No Results")
+      setPitch([])
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const input = e.target[0].value
+
+    getPitch(input)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="Search Japanese Word" />
+        <button type="submit">Search</button>
+      </form>
+      <p>{JSON.stringify(pitch)}</p>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
