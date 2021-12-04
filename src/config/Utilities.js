f7de5a4d4d4b0586df3934ab9ff13e1991bc83e6
Patrick Auri <patrickaulie@gmail.com>
2021-12-04 22:02:54 +0100

    WIP

--- src/config/Utilities.js
+++ src/config/Utilities.js
@@ -9,34 +9,62 @@
 //
 
 export const ParseNHK = (data) => {
-    const dWord = data['Word 1A']
-    const dPitch = data['Pitch 1.1']
+  const dWord = data["Word 1A"]
+  const dPitch = data["Pitch 1.1"]
 
-    let result = { word: '', pitchNumber: -1, reading: [], pitchType: '' }
+  let result = { word: "", pitchNumber: -1, reading: [], pitchType: "" }
 
-    const ParseNHKReading = () => {
-        // Add each character of the reading into an array
-        let a = []
-        for (let i = 0; i < dPitch.length; i++) {
-            if (dPitch[i] !== '') {
-                console.log(`Char: ${dPitch} `)
-                /* if (dPitch[i] === '＼') {
-                    result.pitchNumber = i
-                } else if (dPitch[i] === '￣') {
-                    result.pitchNumber = 0
-                } else {
-                    a.push(dPitch[i])
-                } */
-            }
+  const IsPitchSmallKana = (x) => {
+    const kanaArray = ["ゃ", "ゅ", "ょ"]
+
+    kanaArray.forEach((k) => {
+      if (x === k) return true
+    })
+    return false
+  }
+
+  const ParseNHKWord = () => {
+    const locStart = dWord.indexOf("【") + 1
+    const locEnd = dWord.indexOf("】")
+    result.word = dWord.substring(locStart, locEnd)
+  }
+
+  const ParseNHKReading = () => {
+    const readingKana = dWord.substring(0, dWord.indexOf("【"))
+    let a = []
+
+    if (dPitch.indexOf("￣") === -1) {
+      // Kifukushiki
+      let pitchIndex = 0
+      a = dPitch.split("")
+      a.forEach((e, i) => {
+        if (e.charCodeAt(0) === 12442) {
+          a.splice(i, 1)
         }
-        result.reading = a.toString()
+      })
+      console.table(a)
+    } else {
+      // Heibanshiki
+      result.pitchNumber = 0
     }
 
-    if (dPitch) {
-        ParseNHKReading()
-    }
+    /* for (let i = 0; i < dPitch.length; i++) {
+      if (dPitch[i] !== "") {
 
-    const ParsePitchNumber = () => {}
 
-    return result
+        if (!IsPitchSmallKana(dPitch[i])){
+            pitchIndex++
+        }
+      }
+    } */
+
+    result.reading = a.toString()
+  }
+
+  ParseNHKWord()
+  if (dPitch) ParseNHKReading()
+
+  const ParsePitchNumber = () => {}
+
+  return result
 }
