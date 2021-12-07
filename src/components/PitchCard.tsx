13d2248dfcef9197a5bc43bd961407f921131173
Patrick Auri <patrickaulie@gmail.com>
2021-12-07 22:46:21 +0100

    WIP

--- src/components/PitchCard.tsx
+++ src/components/PitchCard.tsx
@@ -1,18 +1,27 @@
-import { GetPitchTypeName, ParseNHK } from '../config/Utilities'
+import { GetPitchTypeName, ParseNHK } from "../config/Utilities"
 
 export default function PitchCard({ data }: any) {
-    const parsedData = ParseNHK(data)
+  const parsedData = ParseNHK(data)
 
-    return (
-        <div className='card'>
-            <div className='card-top'>
-                <div className='card-title'>{data['Word 1A']}</div>
-                <div className='card-number'>{parsedData.pitchNumber}</div>
-            </div>
-            <div className='card-reading'>{parsedData.reading}</div>
-            <div className='card-bottom'>
-                {GetPitchTypeName(parsedData.pitchType)}
-            </div>
-        </div>
-    )
+  return (
+    <div className="card">
+      <div className="card-top">
+        <div className="card-title">{data["Word 1A"]}</div>
+        <div className="card-number">{parsedData.pitchNumber}</div>
+      </div>
+      <div className="card-reading">
+        <PitchDrawing
+          reading={parsedData.reading}
+          pitchNumber={parsedData.pitchNumber}
+        />
+      </div>
+      <div className="card-bottom">
+        {GetPitchTypeName(parsedData.pitchType)}
+      </div>
+    </div>
+  )
+}
+
+const PitchDrawing = ({ reading, pitchNumber }: any) => {
+  return <div className="">test</div>
 }
